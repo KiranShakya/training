412af077f74152650b4c9c6244e7d1859ad86391
Kiran Shakya <kiran.shakya@itonics.de>
2022-03-03 19:52:50 +0545

    DAY 6

--- src/app/modules/contact/contact-us/contact-us.component.ts
+++ src/app/modules/contact/contact-us/contact-us.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { fromEvent, Subscription } from 'rxjs';
 import { CommonService } from 'src/app/services/common.service';
+
+// Custom Validator
+export function forbiddenNameValidator(nameRe: string): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const forbidden = nameRe.toLocaleLowerCase() !== control.value.toLocaleLowerCase();
+    return forbidden ? {forbiddenName: {value: control.value}} : null;
+  };
+}
 
 @Component({
   selector: 'app-contact-us',
@@ -8,7 +17,22 @@ import { CommonService } from 'src/app/services/common.service';
   styleUrls: ['./contact-us.component.scss'],
 })
 export class ContactUsComponent implements OnInit, OnDestroy {
+  public name: string = 'Angular Training';
+  public email: string = '';
+  public message: string = '';
+  // Reactive Form
+  public contactForm: FormGroup = new FormGroup({
+    name: new FormControl(this.name, [
+      Validators.required, Validators.minLength(3), forbiddenNameValidator('Angular')
+    ]),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    message: new FormControl('')
+  });
+
+
+
   private eventSub: Subscription | undefined;
+  
   
   constructor(private readonly commonService: CommonService) {}
 
@@ -25,5 +49,13 @@ export class ContactUsComponent implements OnInit, OnDestroy {
 
   clickHandler(event: MouseEvent) {
     console.log('Hi');
+    // this.contactForm.setValue({name: new Date().toTimeString(), email: '', message: ''})
+  }
+
+  onSubmitReactive() {
+    console.log('Submitted', this.contactForm.getRawValue());
+  }
+  onSubmit() {
+    console.log('Submitted', this.name, this.email, this.message);
   }
 }
--- src/app/modules/contact/contact.module.ts
+++ src/app/modules/contact/contact.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { ContactRoutingModule } from './contact-routing/contact-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -11,7 +12,10 @@ import { ContactRoutingModule } from './contact-routing/contact-routing.module';
   ],
   imports: [
     CommonModule,
-    ContactRoutingModule
+    ContactRoutingModule,
+    // You may not need both of these following modules
+    ReactiveFormsModule, // For Reactive Forms
+    FormsModule // For Template Driven Forms
   ]
 })
 export class ContactModule { }
